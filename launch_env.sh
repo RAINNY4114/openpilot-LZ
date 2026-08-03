90271777d02bb4131214b7ab59e6af434cb0ff7f
SusanYu25 <yuhong299@gmail.com>
2026-08-03 10:21:50 +0800

    clean nav097 initial commit

---
+++ launch_env.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/bash
+
+export OMP_NUM_THREADS=1
+export MKL_NUM_THREADS=1
+export NUMEXPR_NUM_THREADS=1
+export OPENBLAS_NUM_THREADS=1
+export VECLIB_MAXIMUM_THREADS=1
+
+export ATHENA_HOST='ws://op.caminfo.cn:7776'
+export API_HOST='http://op.caminfo.cn:7777'
+#export MAPBOX_TOKEN=' '
+
+if [ -z "$AGNOS_VERSION" ]; then
+  export AGNOS_VERSION="10.1"
+fi
+
+export STAGING_ROOT="/data/safe_staging"
---
+++ launch_openpilot.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# ==========================
+export ATHENA_HOST='wss://athena.konik.ai/'
+export API_HOST='https://api.konik.ai'
+export MAPBOX_ACCESS_TOKEN=""
+export SKIP_FW_QUERY=1
+export NAVD=true
+export NAVMODEL=true
+
+# ==========================
+
+exec ./launch_chffrplus.sh
